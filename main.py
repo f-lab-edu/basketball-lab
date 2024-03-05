33b5fa1113f9d37babcdaa919eaf34443cec41e3
circuitbreak98 <circuitbreak98@google.com>
2024-03-05 22:21:50 +0900

    init board create API

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
 from fastapi import status
+from pydantic import BaseModel
+
+import sqlalchemy
 import uvicorn 
 
+metadata = sqlalchemy.MetaData()
+
+boards = sqlalchemy.Table(
+    "boards",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("name", sqlalchemy.String),
+    sqlalchemy.Column("description", sqlalchemy.String),
+)
+
+class Board(BaseModel):
+    name: str
+    description: str | None = None
 
 app = FastAPI()
 
@@ -9,6 +25,11 @@ app = FastAPI()
 async def root():
     return {"message":"Hello World"}
 
+@app.post("/boards", status_code=status.HTTP_201_CREATED)
+async def create_board(board: Board):
+    query = boards.insert().values(name=board.name, description=board.description)
+    
+    return board
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000,
