de50db39cab0efbdfd1ffb2a0257083a2cc10789
seonho.lee <seonho.lee@formalworks.com>
2024-03-21 18:26:04 +0900

    init board PATCH API

--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,8 @@ from app.main import app
 import time 
 
 client = TestClient(app)
-def create_board_name_and_response():
+
+def create_board_name_and_response(id=0):
     time_sec = int(time.time())
     response = client.post("/boards/", json={
         "name": "board"+str(time_sec),
@@ -42,6 +43,7 @@ def test_retrieve_board_not_exist():
     response = client.get("/boards/"+str(time_sec))
     assert response.status_code == 404
 
+
 def test_retrieve_all_boards():
     _, response = create_board_name_and_response()
     response = client.get("/boards/")
@@ -49,3 +51,19 @@ def test_retrieve_all_boards():
 
 def test_retrieve_all_empty_boards():
     pass
+
+
+def test_modify_board():
+    time.sleep(1) #test 중에 int(time.time())의 값이 같은 경우가 있어 
+                  #결과적으로 create_board_name_and_response의 예외가 발생함.
+                  #근본적으로 해결할 방법이 없을지 생각해볼 필요 있음.
+    _, create_response = create_board_name_and_response()
+    print(create_response.json())
+    response = client.patch("/boards/"+str(create_response.json()["id"]), json={
+        "name": "board_name_modified",
+        "description": "this board description is modified"
+    })
+    assert response.status_code == 200
+    assert response.json()["id"] == create_response.json()["id"]
+    assert response.json()["name"] == "board_name_modified"
+    assert response.json()["description"] == "this board description is modified"
